b746ae3a0a6d00d1460a8814402ebe414339c0ed
liwanglin12 <i@lwl12.com>
2015-01-11 11:50:51 +0800

    新增微软雅黑功能

--- wp-plus.php
+++ wp-plus.php
@@ -1,22 +1,17 @@
 <?php
 /*
 Plugin Name: 博客多功能增强插件
-Plugin URI: http://lwl12.com
+Plugin URI: http://blog.lwl12.com/wp-plus/
 Description: 博客多功能增强插件
 Author: liwanglin12
 Author URI: http://lwl12.com
-Version: 0.0.1
+Version: 0.2
 */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 define( 'WP_NPROGRESS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-/**
- * Enqueue styles and scripts to `head'.
- *
- * @since 0.0.1
- */
 
 //进度条
 function wpn_enqueue() {
@@ -39,7 +34,7 @@ return $avatar;
 }
 add_filter( 'get_avatar', 'mytheme_get_avatar', 10, 3 );
 
-//替换google相关资源
+//替换google相关资源到360前端库CDN
 function izt_cdn_callback($buffer) {
 	return str_replace('googleapis.com', 'useso.com', $buffer);
 }
@@ -52,3 +47,17 @@ function izt_buffer_end() {
 add_action('init', 'izt_buffer_start');
 add_action('shutdown', 'izt_buffer_end');
 
+// 修改后台字体为微软雅黑
+function admin_fonts(){
+    echo'<style type="text/css">
+        * { font-family: "Microsoft YaHei" !important; }
+        i, .ab-icon, .mce-close, i.mce-i-aligncenter, i.mce-i-alignjustify, i.mce-i-alignleft, i.mce-i-alignright, i.mce-i-blockquote, i.mce-i-bold, i.mce-i-bullist, i.mce-i-charmap, i.mce-i-forecolor, i.mce-i-fullscreen, i.mce-i-help, i.mce-i-hr, i.mce-i-indent, i.mce-i-italic, i.mce-i-link, i.mce-i-ltr, i.mce-i-numlist, i.mce-i-outdent, i.mce-i-pastetext, i.mce-i-pasteword, i.mce-i-redo, i.mce-i-removeformat, i.mce-i-spellchecker, i.mce-i-strikethrough, i.mce-i-underline, i.mce-i-undo, i.mce-i-unlink, i.mce-i-wp-media-library, i.mce-i-wp_adv, i.mce-i-wp_fullscreen, i.mce-i-wp_help, i.mce-i-wp_more, i.mce-i-wp_page, .qt-fullscreen, .star-rating .star { font-family: dashicons !important; }
+        .mce-ico { font-family: tinymce, Arial !important; }
+        .fa { font-family: FontAwesome !important; }
+        .genericon { font-family: "Genericons" !important; }
+        .appearance_page_scte-theme-editor #wpbody *, .ace_editor * { font-family: Monaco, Menlo, "Ubuntu Mono", Consolas, source-code-pro, monospace !important; }
+        .post-type-post #advanced-sortables, .post-type-post #autopaging .description { display: none !important; }
+        .form-field input, .form-field textarea { width: inherit; border-width: 0; }
+        </style>';
+}
+add_action('admin_head', 'admin_fonts');
