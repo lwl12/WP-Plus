5e9e14e532e05ad7d49e80900464933096833c67
liwanglin12 <i@lwl12.com>
2015-03-16 21:36:23 +0800

    no message

--- wp-plus.php
+++ wp-plus.php
@@ -5,9 +5,9 @@ Plugin URI: http://blog.lwl12.com/read/wp-plus/
 Description: 博客多功能增强插件
 Author: liwanglin12
 Author URI: http://lwl12.com
-Version: 1.48
+Version: 1.49
 */
-define("plus_version", "1.48");
+define("plus_version", "1.49");
 /* 启用插件自动跳转至设置*/
 function plus_notice(){
 	echo ("您当前的wp-plus版本已经停用，请您到Wordpress官方插件源更新您的插件！");
