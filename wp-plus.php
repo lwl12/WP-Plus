61289dc5ca8b9289d867426d1f322be2b0f3b6bc
liwanglin12 <i@lwl12.com>
2015-12-26 23:00:15 +0800

    Add decodeURI function for Troy

--- wp-plus.php
+++ wp-plus.php
@@ -135,16 +135,7 @@ if (get_option('wp_plus_wryh') == 'checked') {
 <?php
     function plus_admin_fonts()
     {
-        echo '<style type="text/css">
-        * { font-family: "Microsoft YaHei" !important; }
-        i, .ab-icon, .mce-close, i.mce-i-aligncenter, i.mce-i-alignjustify, i.mce-i-alignleft, i.mce-i-alignright, i.mce-i-blockquote, i.mce-i-bold, i.mce-i-bullist, i.mce-i-charmap, i.mce-i-forecolor, i.mce-i-fullscreen, i.mce-i-help, i.mce-i-hr, i.mce-i-indent, i.mce-i-italic, i.mce-i-link, i.mce-i-ltr, i.mce-i-numlist, i.mce-i-outdent, i.mce-i-pastetext, i.mce-i-pasteword, i.mce-i-redo, i.mce-i-removeformat, i.mce-i-spellchecker, i.mce-i-strikethrough, i.mce-i-underline, i.mce-i-undo, i.mce-i-unlink, i.mce-i-wp-media-library, i.mce-i-wp_adv, i.mce-i-wp_fullscreen, i.mce-i-wp_help, i.mce-i-wp_more, i.mce-i-wp_page, .qt-fullscreen, .star-rating .star { font-family: dashicons !important; }
-        .mce-ico { font-family: tinymce, Arial !important; }
-        .fa { font-family: FontAwesome !important; }
-        .genericon { font-family: "Genericons" !important; }
-        .appearance_page_scte-theme-editor #wpbody *, .ace_editor * { font-family: Monaco, Menlo, "Ubuntu Mono", Consolas, source-code-pro, monospace !important; }
-        .post-type-post #advanced-sortables, .post-type-post #autopaging .description { display: none !important; }
-        .form-field input, .form-field textarea { width: inherit; border-width: 0; }
-        </style>';
+        echo '<style type="text/css"> * { font-family: "Microsoft YaHei" !important; } i, .ab-icon, .mce-close, i.mce-i-aligncenter, i.mce-i-alignjustify, i.mce-i-alignleft, i.mce-i-alignright, i.mce-i-blockquote, i.mce-i-bold, i.mce-i-bullist, i.mce-i-charmap, i.mce-i-forecolor, i.mce-i-fullscreen, i.mce-i-help, i.mce-i-hr, i.mce-i-indent, i.mce-i-italic, i.mce-i-link, i.mce-i-ltr, i.mce-i-numlist, i.mce-i-outdent, i.mce-i-pastetext, i.mce-i-pasteword, i.mce-i-redo, i.mce-i-removeformat, i.mce-i-spellchecker, i.mce-i-strikethrough, i.mce-i-underline, i.mce-i-undo, i.mce-i-unlink, i.mce-i-wp-media-library, i.mce-i-wp_adv, i.mce-i-wp_fullscreen, i.mce-i-wp_help, i.mce-i-wp_more, i.mce-i-wp_page, .qt-fullscreen, .star-rating .star { font-family: dashicons !important; } .mce-ico { font-family: tinymce, Arial !important; } .fa { font-family: FontAwesome !important; } .genericon { font-family: "Genericons" !important; } .appearance_page_scte-theme-editor #wpbody *, .ace_editor * { font-family: Monaco, Menlo, "Ubuntu Mono", Consolas, source-code-pro, monospace !important; } .post-type-post #advanced-sortables, .post-type-post #autopaging .description { display: none !important; } .form-field input, .form-field textarea { width: inherit; border-width: 0; } </style>';
     }
     add_action('admin_head', 'plus_admin_fonts');
 ?>
